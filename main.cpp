c8759a621f0bfe44beec844c30726d64c6106fc3
Victor Aymerich Quesada <vaymerich@estudiantec.cr>
2023-09-24 23:17:37 -0600

    Structs Constructores y Producto
    Se agregaron los structs de constructores y productores, y unos métodos para lograr su funcionamiento.

--- main.cpp
+++ main.cpp
@@ -11,4 +11,20 @@ int main(int argc, char** argv) {
     cout << "\nLista de strings compleja:" << endl;
     ListaCompleja *lista2 = SepararStringsPorLineas(linea, "Cliente");
     lista2->imprimir();
+
+    Producto *producto = new Producto("101", 100 , 30 ,"A" ,"X10");
+    producto->imprimir();
+    ListaSimple * Lista = producto->ConvertirEnListaSimple();
+    Lista->imprimir();
+    cout << "----------------------------------------------------------------------------------" << endl;
+    ListaCompleja * listaDeProductos = new ListaCompleja();
+    listaDeProductos->agregar("Producto", Lista);
+    listaDeProductos->imprimir();
+    Constructor * constructor = new Constructor("C1", 0, "101");
+    constructor->AgregarCantidadAlProducto(listaDeProductos);
+    listaDeProductos->imprimir();
+
+
+
+
 }
